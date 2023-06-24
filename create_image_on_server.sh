ce8a0bf5cf61157c13d8ae2739547f56b9d25f74
Sérgio Gomes <srochg@gmail.com>
2023-06-23 23:06:48 -0300

    configuring environment

--- create_image_on_server.sh
+++ create_image_on_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 if [ -z $1 ] || [ -z $2 ]; then
     echo "Erro: É necessário informar o usuario e senha do dockerhub como parâmetros."
     exit 1
@@ -10,15 +13,16 @@ DOCKER_PASSWORD=$2
 DOCKER_HUB_USERNAME="srochg"
 DOCKER_HUB_REPO=$(mvn help:evaluate -Dexpression=project.name -q -DforceStdout)
 DOCKER_HUB_TAG=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+name_image="$DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO:$DOCKER_HUB_TAG"
 
 # Construa a imagem Docker
-sudo docker build -t "$DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO:$DOCKER_HUB_TAG" .
+sudo docker build -t $name_image .
 
 # Faça o login no Docker Hub
 echo "${DOCKER_PASSWORD}" | sudo docker login -u="${DOCKER_USERNAME}" --password-stdin
 
 # Faça o push da imagem para o Docker Hub
-sudo docker push "$DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO:$DOCKER_HUB_TAG"
+sudo docker push $name_image
 
 # Limpeza: Remova a imagem localmente, se desejar
-sudo docker image rm "$DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO:$DOCKER_HUB_TAG"
+sudo docker image rm $name_image
